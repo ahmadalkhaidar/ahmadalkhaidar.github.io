6bad15c5ede62ef8726fe1e9311c38a3518871b3
ahmadalkhaidar <ahmadalkhaidar2@gmail.com>
2024-08-22 15:34:10 +0700

    first commit

---
+++ src/data/projects.js
@@ -0,0 +1,306 @@
+// Dimension Recomended: 780x680
+import Funnelspro from '../images/project/funnelspro.jpg';
+import Funnelspro1 from '../images/project/funnelspro-1.png';
+import Funnelspro2 from '../images/project/funnelspro-2.png';
+import Funnelspro3 from '../images/project/funnelspro-3.png';
+import Funnelspro4 from '../images/project/funnelspro-4.png';
+
+import MbkmCMS from '../images/project/mbkm-cms.png';
+import MbkmCMS1 from '../images/project/mbkm-cms-1.png';
+import MbkmCMS2 from '../images/project/mbkm-cms-2.png';
+import MbkmCMS3 from '../images/project/mbkm-cms-3.png';
+import MbkmCMS4 from '../images/project/mbkm-cms-4.png';
+
+import UCCareerCenterApp from '../images/project/uc_careercenter.jpg';
+import UCCareerCenterImage1 from '../images/project/uc_career-7.png';
+import UCCareerCenterImage2 from '../images/project/uc_career-8.png';
+import UCCareerCenterImage3 from '../images/project/uc_career-9.png';
+import UCCareerCenterImage4 from '../images/project/uc_career-10.png';
+
+// import PropanSalesForce from '../images/project/propan_sales_analyzer.jpg';
+
+export const projectsData = [
+	{
+		id: 1,
+		title: 'Funnelspro',
+		category: 'Web Application',
+		img: Funnelspro,
+		ProjectHeader: {
+			title: 'Funnelspro',
+			tags: 'Full Stack / Web Application',
+		},
+		ProjectImages: [
+			{
+				id: 1,
+				title: null,
+				img: Funnelspro1,
+			},
+			{
+				id: 2,
+				title: null,
+				img: Funnelspro2,
+			},
+			{
+				id: 3,
+				title: null,
+				img: Funnelspro3,
+			},
+			{
+				id: 4,
+				title: null,
+				img: Funnelspro4,
+			},
+		],
+		ProjectInfo: {
+			ClientHeading: 'About Project',
+			CompanyInfo: [
+				{
+					id: 1,
+					title: 'Name',
+					details: 'Funnelspro',
+				},
+				{
+					id: 2,
+					title: 'As',
+					details: 'Full Stack Developer',
+				},
+				{
+					id: 3,
+					title: 'Website',
+					details: 'https://tools.dropshipaja.com',
+					link: 'https://tools.dropshipaja.com',
+				},
+			],
+			ObjectivesHeading: 'Objective',
+			ObjectivesDetails:
+				'FunnelsPro is a software for creating cool website pages. FunnelsPro is designed to help business owners like you, who are not programmers and cannot code, to develop their online businesses to the next level.',
+			Technologies: [
+				{
+					title: 'Tools & Technologies',
+					techs: [
+						'HTML',
+						'CSS',
+						'JavaScript',
+						'PHP',
+						'MySQL',
+						'Bootstrap',
+						'GrapeJS',
+						'CodeIgniter 3',
+						'others.',
+					],
+				},
+			],
+			ProjectDetailsHeading: 'Description',
+			ProjectDetails: [
+				{
+					id: 1,
+					details: `A landing page builder system that integrates with various advertising tools, such as Google Ads, Facebook Ads, TikTok Ads and others, allows users to create optimized landing pages and manage their ad campaigns seamlessly. The system can connect with external domains, enabling businesses to use their own domain names for these landing pages. For subscription payments, the system utilizes the Midtrans payment gateway, offering a secure and efficient way for users to subscribe to services directly through the platform.`
+				},
+			],
+			SocialSharingHeading: '',
+			SocialSharing: [
+				
+			],
+		},
+		RelatedProject: {
+			title: 'Related Projects',
+			Projects: [
+			],
+		},
+	},
+	{
+		id: 4,
+		title: 'MBKM CMS UPJ',
+		category: 'Web Application',
+		img: MbkmCMS,
+		ProjectHeader: {
+			title: 'MBKM CMS',
+			publishDate: 'Jan 26, 2023',
+			tags: 'Full Stack / Web Application',
+		},
+		ProjectImages: [
+			{
+				id: 1,
+				title: null,
+				img: MbkmCMS,
+			},
+			{
+				id: 2,
+				title: null,
+				img: MbkmCMS1,
+			},
+			{
+				id: 3,
+				title: null,
+				img: MbkmCMS2,
+			},
+			{
+				id: 4,
+				title: null,
+				img: MbkmCMS3,
+			},
+			{
+				id: 5,
+				title: null,
+				img: MbkmCMS4,
+			},
+		],
+		ProjectInfo: {
+			ClientHeading: 'About Project',
+			CompanyInfo: [
+				{
+					id: 1,
+					title: 'Name',
+					details: 'MBKM CMS UPJ',
+				},
+				{
+					id: 2,
+					title: 'As',
+					details: 'Full Stack Developer',
+				},
+				{
+					id: 3,
+					title: 'Website',
+					details: 'https://mbkm.upj.ac.id',
+					link: 'https://mbkm.upj.ac.id',
+				},
+			],
+			ObjectivesHeading: 'Objective',
+			ObjectivesDetails:
+				'Designing and building MBKM CMS system at Universitas Pembangunan Jaya with a reliable content management system module and designing applications for students.',
+			Technologies: [
+				{
+					title: 'Tools & Technologies',
+					techs: [
+						'HTML',
+						'CSS',
+						'JavaScript',
+						'PHP',
+						'MySQL',
+						'Bootstrap',
+						'CodeIgniter 3',
+						'others.',
+					],
+				},
+			],
+			ProjectDetailsHeading: 'Description',
+			ProjectDetails: [
+				{
+					id: 4,
+					details: ``
+				},
+			],
+			SocialSharingHeading: '',
+			SocialSharing: [
+				
+			],
+		},
+		RelatedProject: {
+			title: 'Related Projects',
+			Projects: [
+				{
+					id: 5,
+					title: 'Career Center UC',
+					img: UCCareerCenterApp,
+					link: '/projects/5',
+				}
+			],
+		},
+	},
+	{
+		id: 5,
+		title: 'Career Center UC',
+		category: 'Web Application',
+		img: UCCareerCenterApp,
+		ProjectHeader: {
+			title: 'Career Center UC',
+			publishDate: 'May 13, 2023',
+			tags: 'Full Stack / Web Application',
+		},
+		ProjectImages: [
+			{
+				id: 1,
+				title: null,
+				img: UCCareerCenterImage1,
+			},
+			{
+				id: 2,
+				title: null,
+				img: UCCareerCenterImage2,
+			},
+			{
+				id: 3,
+				title: null,
+				img: UCCareerCenterImage3,
+			},
+			{
+				id: 4,
+				title: null,
+				img: UCCareerCenterImage4,
+			},
+		],
+		ProjectInfo: {
+			ClientHeading: 'About Project',
+			CompanyInfo: [
+				{
+					id: 1,
+					title: 'Name',
+					details: 'Career Center UC',
+				},
+				{
+					id: 2,
+					title: 'As',
+					details: 'Full Stack Developer',
+				},
+				{
+					id: 3,
+					title: 'Website',
+					details: 'https://careercenter.uc.ac.id',
+					link: 'https://careercenter.dkproject.my.id/'
+				},
+			],
+			ObjectivesHeading: 'Objective',
+			ObjectivesDetails:
+				'Designing and building a Career Center system equipped with a CMS module. This application is made specifically for Ciputra University student alumni.',
+			Technologies: [
+				{
+					title: 'Tools & Technologies',
+					techs: [
+						'HTML',
+						'CSS',
+						'JavaScript',
+						'PHP',
+						'MySQL',
+						'Bootstrap',
+						'CodeIgniter 3',
+						'others.',
+					],
+				},
+			],
+			ProjectDetailsHeading: 'Description',
+			ProjectDetails: [
+				{
+					id: 1,
+					details: ``,
+				}
+			],
+			SocialSharingHeading: '',
+			SocialSharing: [
+				
+			],
+		},
+		RelatedProject: {
+			title: 'Related Projects',
+			Projects: [
+				{
+					id: 4,
+					title: 'MBKM CMS UPJ',
+					img: MbkmCMS,
+					link: '/projects/4',
+				}
+			],
+		},
+	},
+];
+
+export default projectsData;
