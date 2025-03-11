0ac3fc791c145a951803003822b1a7b119df9e2f
Ahmad Al Khaidar <ahmadalkhaidar2@gmail.com>
2025-03-11 09:50:53 +0700

    update

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,12 +5,6 @@ import Funnelspro2 from '../images/project/funnelspro-2.png';
 import Funnelspro3 from '../images/project/funnelspro-3.png';
 import Funnelspro4 from '../images/project/funnelspro-4.png';
 
-import MbkmCMS from '../images/project/mbkm-cms.png';
-import MbkmCMS1 from '../images/project/mbkm-cms-1.png';
-import MbkmCMS2 from '../images/project/mbkm-cms-2.png';
-import MbkmCMS3 from '../images/project/mbkm-cms-3.png';
-import MbkmCMS4 from '../images/project/mbkm-cms-4.png';
-
 import UCCareerCenterApp from '../images/project/uc_careercenter.jpg';
 import UCCareerCenterImage1 from '../images/project/uc_career-7.png';
 import UCCareerCenterImage2 from '../images/project/uc_career-8.png';
@@ -29,6 +23,40 @@ import UCINCAD1 from '../images/project/uc_incad-1.png';
 import UCINCAD2 from '../images/project/uc_incad-2.png';
 import UCINCAD3 from '../images/project/uc_incad-3.png';
 import UCINCAD4 from '../images/project/uc_incad-4.png';
+
+import MbkmCMS from '../images/project/mbkm-cms.png';
+import MbkmCMS1 from '../images/project/mbkm-cms-1.png';
+import MbkmCMS2 from '../images/project/mbkm-cms-2.png';
+import MbkmCMS3 from '../images/project/mbkm-cms-3.png';
+import MbkmCMS4 from '../images/project/mbkm-cms-4.png';
+
+import UPJEOFFICEApp from '../images/project/upj_e-office.png';
+import UPJEOFFICE1 from '../images/project/upj_e-office-1.png';
+import UPJEOFFICE2 from '../images/project/upj_e-office-2.png';
+import UPJEOFFICE3 from '../images/project/upj_e-office-3.png';
+import UPJEOFFICE4 from '../images/project/upj_e-office-4.png';
+import UPJEOFFICE5 from '../images/project/upj_e-office-5.png';
+import UPJEOFFICE6 from '../images/project/upj_e-office-6.png';
+
+import UPJSIMPULApp from '../images/project/upj_simpul.jpeg';
+import UPJSIMPUL1 from '../images/project/upj_simpul-1.png';
+import UPJSIMPUL2 from '../images/project/upj_simpul-2.png';
+import UPJSIMPUL3 from '../images/project/upj_simpul-3.png';
+import UPJSIMPUL4 from '../images/project/upj_simpul-4.png';
+import UPJSIMPUL5 from '../images/project/upj_simpul-5.png';
+import UPJSIMPUL6 from '../images/project/upj_simpul-6.png';
+import UPJSIMPUL7 from '../images/project/upj_simpul-7.png';
+import UPJSIMPUL8 from '../images/project/upj_simpul-8.png';
+import UPJSIMPUL9 from '../images/project/upj_simpul-9.png';
+
+import UPJSPAApp from '../images/project/upj_spa.jpeg';
+import UPJSPA1 from '../images/project/upj_spa-1.png';
+import UPJSPA2 from '../images/project/upj_spa-2.png';
+import UPJSPA3 from '../images/project/upj_spa-3.png';
+import UPJSPA4 from '../images/project/upj_spa-4.png';
+import UPJSPA5 from '../images/project/upj_spa-5.png';
+import UPJSPA6 from '../images/project/upj_spa-6.png';
+import UPJSPA7 from '../images/project/upj_spa-7.png';
 
 // import PropanSalesForce from '../images/project/propan_sales_analyzer.jpg';
 
@@ -548,7 +576,7 @@ export const projectsData = [
 			ProjectDetailsHeading: 'Challenges',
 			ProjectDetails: [
 				{
-					id: 4,
+					id: 1,
 					details: `<ul style="list-style:disc;padding-inline-start:30px;font-weight:normal;">
 								<li>
 									<b>Requirement Analysis:</b> The first step in designing a CMS is to analyze the requirements of the system. This includes understanding the needs of the users, the features they require, and the goals of the system.
@@ -578,9 +606,505 @@ export const projectsData = [
 			Projects: [
 				{
 					id: 6,
+					title: 'E-OFFICE UPJ',
+					img: UPJEOFFICEApp,
+					link: '/projects/6',
+				},
+				{
+					id: 7,
+					title: 'SiMPUL UPJ',
+					img: UPJSIMPULApp,
+					link: '/projects/7',
+				},
+				{
+					id: 8,
+					title: 'SPA UPJ',
+					img: UPJSPAApp,
+					link: '/projects/8',
+				}
+			],
+		},
+	},
+	{
+		id: 6,
+		title: 'E-Office UPJ',
+		category: 'Web Application',
+		img: UPJEOFFICEApp,
+		ProjectHeader: {
+			title: 'E-Office UPJ',
+			publishDate: 'October 12, 2022',
+			tags: 'Full Stack / Web Application',
+		},
+		ProjectImages: [
+			{
+				id: 1,
+				title: null,
+				img: UPJEOFFICE1,
+			},
+			{
+				id: 2,
+				title: null,
+				img: UPJEOFFICE2,
+			},
+			{
+				id: 3,
+				title: null,
+				img: UPJEOFFICE3,
+			},
+			{
+				id: 4,
+				title: null,
+				img: UPJEOFFICE4,
+			},
+			{
+				id: 5,
+				title: null,
+				img: UPJEOFFICE5,
+			},
+			{
+				id: 6,
+				title: null,
+				img: UPJEOFFICE6,
+			},
+		],
+		ProjectInfo: {
+			ClientHeading: 'About Project',
+			CompanyInfo: [
+				{
+					id: 1,
+					title: 'Name',
+					details: 'E-Office UPJ',
+				},
+				{
+					id: 2,
+					title: 'As',
+					details: 'Full Stack Developer',
+				},
+				{
+					id: 3,
+					title: 'Website',
+					details: 'https://e-office.upj.ac.id',
+					link: 'https://e-office.upj.ac.id',
+				},
+			],
+			ObjectivesHeading: 'Objective',
+			ObjectivesDetails:
+				'Designing and building an E-Office system at Pembangunan Jaya University which is used to manage the process of borrowing infrastructure, borrowing equipment and submitting documents.',
+			Technologies: [
+				{
+					title: 'Tools & Technologies',
+					techs: [
+						'HTML',
+						'CSS',
+						'JavaScript',
+						'PHP',
+						'MySQL',
+						'Bootstrap',
+						'CodeIgniter 3',
+						'others.',
+					],
+				},
+			],
+			ProjectDetailsHeading: 'Challenges',
+			ProjectDetails: [
+				{
+					id: 1,
+					details: `<ol style="list-style: decimal;margin-left:20px;font-size:14px;">
+								<li>
+									<b>
+									Designing a User-Friendly Interface:
+									</b>
+									<br>
+									Designing a user-friendly interface for the system that is easy to use and navigate for employees and students
+								</li>
+								<li>
+								<b>Developing a Robust Backend:</b>
+								<br>
+								Developing a robust backend system that can handle large amounts of data and user requests efficiently
+								</li>
+								<li>
+									<b>Ensuring Data Security:</b>
+									<br>
+									Ensuring the security and integrity of employee data, student data and performance records through encryption and access controls.
+								</li>
+								<li>
+									<b>Testing and Deployment:</b>
+									<br>
+									Testing the system to ensure it works as expected and deploying it to the production environment.
+								</li>
+								<li>
+									<b>Documentation and Maintenance:</b>
+									<br>
+									Providing documentation for the system and ensuring it is maintained and updated regularly.
+								</li>
+								<li>
+									<b>User Training and Support:</b>
+									<br>
+									Providing training and support for users to ensure they can use the system effectively.
+								</li>
+							</ol>`
+				},
+			],
+			SocialSharingHeading: '',
+			SocialSharing: [
+				
+			],
+		},
+		RelatedProject: {
+			title: 'Related Projects',
+			Projects: [
+				{
+					id: 5,
 					title: 'MBKM UPJ',
 					img: MbkmCMS,
+					link: '/projects/5',
+				},
+				{
+					id: 7,
+					title: 'SiMPUL UPJ',
+					img: UPJSIMPULApp,
+					link: '/projects/7',
+				},
+				{
+					id: 8,
+					title: 'SPA UPJ',
+					img: UPJSPAApp,
 					link: '/projects/6',
+				}
+			],
+		},
+	},
+	{
+		id: 7,
+		title: 'SiMPUL UPJ',
+		category: 'Web Application',
+		img: UPJSIMPULApp,
+		ProjectHeader: {
+			title: 'SiMPUL UPJ',
+			publishDate: 'November 07, 2023',
+			tags: 'Full Stack / Web Application',
+		},
+		ProjectImages: [
+			{
+				id: 1,
+				title: null,
+				img: UPJSIMPUL1,
+			},
+			{
+				id: 2,
+				title: null,
+				img: UPJSIMPUL2,
+			},
+			{
+				id: 3,
+				title: null,
+				img: UPJSIMPUL3,
+			},
+			{
+				id: 4,
+				title: null,
+				img: UPJSIMPUL4,
+			},
+			{
+				id: 5,
+				title: null,
+				img: UPJSIMPUL5,
+			},
+			{
+				id: 6,
+				title: null,
+				img: UPJSIMPUL6,
+			},
+			{
+				id: 7,
+				title: null,
+				img: UPJSIMPUL7,
+			},
+			{
+				id: 8,
+				title: null,
+				img: UPJSIMPUL8,
+			},
+			{
+				id: 9,
+				title: null,
+				img: UPJSIMPUL9,
+			},
+		],
+		ProjectInfo: {
+			ClientHeading: 'About Project',
+			CompanyInfo: [
+				{
+					id: 1,
+					title: 'Name',
+					details: 'Sistem Kepegawaian Unggul UPJ',
+				},
+				{
+					id: 2,
+					title: 'As',
+					details: 'Full Stack Developer',
+				},
+				{
+					id: 3,
+					title: 'Website',
+					details: 'https://simpul2.upj.ac.id',
+					link: 'https://simpul2.upj.ac.id',
+				},
+			],
+			ObjectivesHeading: 'Objective',
+			ObjectivesDetails:
+				'Designing and building Sistem Kepegawaian Unggul system at Universitas Pembangunan Jaya which is used to manage personnel processes, such as Attendance, Leave and Permit Application, KPI Assessment Process and Form Submission.',
+			Technologies: [
+				{
+					title: 'Tools & Technologies',
+					techs: [
+						'HTML',
+						'CSS',
+						'JavaScript',
+						'PHP',
+						'MySQL',
+						'Bootstrap',
+						'CodeIgniter 3',
+						'others.',
+					],
+				},
+			],
+			ProjectDetailsHeading: 'Challenges',
+			ProjectDetails: [
+				{
+					id: 1,
+					details: `<ol style="list-style: decimal;margin-left:20px;font-size:14px;">
+								<li>
+									<b>
+									Designing a User-Friendly Interface:
+									</b>
+									<br>
+									Designing a user-friendly interface for the system that is easy to use and navigate for employees
+								</li>
+								<li>
+								<b>Developing a Robust Backend:</b>
+								<br>
+								Developing a robust backend system that can handle large amounts of data and user requests efficiently
+								</li>
+								<li>
+									<b>Ensuring Data Security:</b>
+									<br>
+									Ensuring the security and integrity of employee data and performance records through encryption and access controls.
+								</li>
+								<li>
+									<b>Testing and Deployment:</b>
+									<br>
+									Testing the system to ensure it works as expected and deploying it to the production environment.
+								</li>
+								<li>
+									<b>Documentation and Maintenance:</b>
+									<br>
+									Providing documentation for the system and ensuring it is maintained and updated regularly.
+								</li>
+								<li>
+									<b>User Training and Support:</b>
+									<br>
+									Providing training and support for users to ensure they can use the system effectively.
+								</li>
+							</ol>`
+				},
+			],
+			SocialSharingHeading: '',
+			SocialSharing: [
+				
+			],
+		},
+		RelatedProject: {
+			title: 'Related Projects',
+			Projects: [
+				{
+					id: 5,
+					title: 'MBKM UPJ',
+					img: MbkmCMS,
+					link: '/projects/5',
+				},
+				{
+					id: 6,
+					title: 'E-OFFICE UPJ',
+					img: UPJEOFFICEApp,
+					link: '/projects/6',
+				},
+				{
+					id: 8,
+					title: 'SPA UPJ',
+					img: UPJSPAApp,
+					link: '/projects/8',
+				}
+			],
+		},
+	},
+	{
+		id: 8,
+		title: 'SPA UPJ',
+		category: 'Web Application',
+		img: UPJSPAApp,
+		ProjectHeader: {
+			title: 'SPA UPJ',
+			publishDate: 'March 05, 2023',
+			tags: 'Full Stack / Web Application',
+		},
+		ProjectImages: [
+			{
+				id: 1,
+				title: null,
+				img: UPJSPA1,
+			},
+			{
+				id: 2,
+				title: null,
+				img: UPJSPA2,
+			},
+			{
+				id: 3,
+				title: null,
+				img: UPJSPA3,
+			},
+			{
+				id: 4,
+				title: null,
+				img: UPJSPA4,
+			},
+			{
+				id: 5,
+				title: null,
+				img: UPJSPA5,
+			},
+			{
+				id: 6,
+				title: null,
+				img: UPJSPA6,
+			},
+			{
+				id: 7,
+				title: null,
+				img: UPJSPA7,
+			},
+		],
+		ProjectInfo: {
+			ClientHeading: 'About Project',
+			CompanyInfo: [
+				{
+					id: 1,
+					title: 'Name',
+					details: 'Sistem Pencairan Anggaran UPJ',
+				},
+				{
+					id: 2,
+					title: 'As',
+					details: 'Full Stack Developer',
+				},
+				{
+					id: 3,
+					title: 'Website',
+					details: 'https://spa.upj.ac.id',
+					link: 'https://spa.upj.ac.id',
+				},
+			],
+			ObjectivesHeading: 'Objective',
+			ObjectivesDetails:
+				'Designing and building Sistem Pencairan Anggaran system at Universitas Pembangunan Jaya which is used to manage the budget disbursement process.',
+			Technologies: [
+				{
+					title: 'Tools & Technologies',
+					techs: [
+						'HTML',
+						'CSS',
+						'JavaScript',
+						'PHP',
+						'MySQL',
+						'Bootstrap',
+						'CodeIgniter 3',
+						'others.',
+					],
+				},
+			],
+			ProjectDetailsHeading: 'Challenges',
+			ProjectDetails: [
+				{
+					id: 1,
+					details: `<ol style="list-style: decimal;margin-left:20px;font-size:14px;">
+								<li>
+									<b>
+									Designing a User-Friendly Interface:
+									</b>
+									<br>
+									Designing a user-friendly interface for the Sistem Pencairan Anggaran system that is easy to navigate and use.
+								</li>
+								<li>	
+									<b>
+									Implementing Features for Budget Management:
+									</b>
+									<br>
+									Implementing features for budget management, including budget allocation, disbursement, and tracking.
+								</li>
+								<li>
+									<b>
+									Ensuring Data Security and Integrity:
+									</b>
+									<br>
+									Ensuring data security and integrity by implementing appropriate security measures and data validation.
+								</li>
+								<li>
+									<b>
+									Testing and Deployment:
+									</b>
+									<br>
+									Testing the system to ensure it works as expected and deploying it to the production environment.
+								</li>
+								<li>
+									<b>
+									Documentation and Maintenance:
+									</b>
+									<br>
+									Providing documentation for the system and ensuring it is maintained and updated regularly.
+								</li>
+								<li>
+									<b>
+									User Training and Support:
+									</b>
+									<br>
+									Providing training and support for users to ensure they can use the system effectively.
+								</li>
+								<li>
+									<b>
+									Integration with Existing Systems:
+									</b>
+									<br>
+									Ensuring the Sistem Pencairan Anggaran system integrates seamlessly with existing systems and processes.
+								</li>
+							</ol>`
+				},
+			],
+			SocialSharingHeading: '',
+			SocialSharing: [
+				
+			],
+		},
+		RelatedProject: {
+			title: 'Related Projects',
+			Projects: [
+				{
+					id: 5,
+					title: 'MBKM UPJ',
+					img: MbkmCMS,
+					link: '/projects/5',
+				},
+				{
+					id: 6,
+					title: 'E-OFFICE UPJ',
+					img: UPJSPAApp,
+					link: '/projects/6',
+				},
+				{
+					id: 7,
+					title: 'SiMPUL UPJ',
+					img: UPJSIMPULApp,
+					link: '/projects/7',
 				}
 			],
 		},
